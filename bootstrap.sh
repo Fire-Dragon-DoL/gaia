b891175e9f6759b231d067512ce2ee69674dcb4c
Francesco Belladonna <francesco@fc5.me>
2025-10-19 18:42:22 -0700

    feat: bootstrap script is present
    Executable bootstrap script from curl is present

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+decho() { echo "[Hades] " "$@" >&2 }
+
+set -euo pipefail
+mkdir -p ~/Code
+git clone git@github.com:Fire-Dragon-DoL/hades.git ~/Code/hades
+
+decho "Bootstrap succeeded"
+exit 0
