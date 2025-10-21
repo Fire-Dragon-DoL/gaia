ed065494c79543f7dd8e1cf31311c2d7a6beba1b
Francesco Belladonna <francesco@fc5.me>
2025-10-20 22:15:20 -0700

    feat(bootstrap): chezmoi is installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+CHEZMOI_SOURCE_DIR="$HOME/.local/share/chezmoi"
+GITHUB_USERNAME="Fire-Dragon-DoL"
+CHEZMOI_GITHUB_REPO="https://github.com/$GITHUB_USERNAME/hades.git"
+
 decho() {
   echo "[Hades] " "$@" >&2
 }
 
-set -euo pipefail
-mkdir -p ~/Code
-HADES_REPO_PATH="$(mktemp -d /tmp/hades-XXXXXXXXXX)"
-git clone 'https://github.com/Fire-Dragon-DoL/hades.git' "$HADES_REPO_PATH"
+is-installed() {
+  if command -v "$1" &> /dev/null; then
+    decho "$1 already installed"
+    return 0
+  fi
+
+  return 1
+}
+
+pacman-install-missing() {
+  is-installed "$1" || pacman -S "$1"
+}
+
+pacman-install-missing 'chezmoi'
+[[ -d "$CHEZMOI_SOURCE_DIR" ]] || chezmoi init "$CHEZMOI_GITHUB_REPO"
 
 decho "Bootstrap succeeded"
 exit 0
