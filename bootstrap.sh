0f9d668177597cecfaa94fa245af47739b82b584
Francesco Belladonna <francesco@fc5.me>
2025-10-27 21:41:10 -0700

    feat(bootstrap): supports SSH key from 1password is present

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 CHEZMOI_SOURCE_DIR="$HOME/.local/share/chezmoi"
 GITHUB_USERNAME="Fire-Dragon-DoL"
-CHEZMOI_GITHUB_REPO="https://github.com/$GITHUB_USERNAME/gaia.git"
+CHEZMOI_GITHUB_REPO="git@github.com:${GITHUB_USERNAME}/gaia.git"
 
 decho() {
   echo "[Gaia] " "$@" >&2
@@ -25,12 +25,16 @@ pacman-install-missing() {
 
 is-installed 'wl-copy' || sudo pacman -S 'wl-clipboard'
 pacman-install-missing 'chezmoi'
-[[ -d "$CHEZMOI_SOURCE_DIR" ]] || chezmoi init "$CHEZMOI_GITHUB_REPO"
 
 mkdir -p "$HOME/.ssh"
 chmod 0700 "$HOME/.ssh"
+if [[ ! -f "$HOME/.ssh/id_ed25519" ]]; then
+  read -p "Export id_ed25519 and id_ed25519.pub from 1Password into .ssh and press ENTER"
+fi
 [[ -f "$HOME/.ssh/id_ed25519" ]] && chmod 0600 "$HOME/.ssh/id_ed25519"
 [[ -f "$HOME/.ssh/id_ed25519.pub" ]] && chmod 0644 "$HOME/.ssh/id_ed25519.pub"
+
+[[ -d "$CHEZMOI_SOURCE_DIR" ]] || chezmoi init --ssh "$CHEZMOI_GITHUB_REPO"
 
 decho "Bootstrap succeeded"
 exit 0
